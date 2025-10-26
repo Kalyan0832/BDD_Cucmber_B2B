2a8b2d70b9740c2549c059d9018655548bbc14af
kalyan0832 <spk9640@gmail.com>
2025-10-26 12:12:27 +0530

    New changes

--- CucumberFramework/src/test/java/hook/Hooks.java
+++ CucumberFramework/src/test/java/hook/Hooks.java
@@ -6,10 +6,8 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.Reporter;
 
 import io.cucumber.java.After;
-import io.cucumber.java.AfterAll;
 import io.cucumber.java.AfterStep;
 import io.cucumber.java.Before;
 import io.cucumber.java.BeforeAll;
